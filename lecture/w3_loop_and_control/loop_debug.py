583cafdd35af5f4a320b3b9a5a950b91263623ee
Sungchul Choi <sc82.choi@gachon.ac.kr>
2014-10-03 11:25:06 +0900

    Update chapter 4 examples: loop_debug.py

---
+++ lecture/w3_loop_and_control/loop_debug.py
@@ -0,0 +1,24 @@
+print "input decimal number: ",
+decimal = int(raw_input()) 
+result ="" 
+loop_counter = 0
+while (decimal > 0):
+	temp_decimal_input = decimal
+	temp_result_input=result
+
+	remainder = decimal % 2
+	decimal = decimal / 2
+	result = str(remainder) + result
+
+	print "-----------", loop_counter, "loop value check -------- "
+	print "Initial decimal:", temp_decimal_input,
+	print ", Remainder:",remainder,
+	print ", Inital result", temp_result_input
+	print "Output decimal:", decimal,
+	print "Output result:", result
+	print "------------------------------------------------------"
+	print ""
+
+	loop_counter += 1
+print "Binary number is", result
+
