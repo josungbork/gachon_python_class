aed6174f2a8c315f46d7026cd3a8cd1e6a60908d
blissray <sc82.choi@gachon.ac.kr>
2014-11-09 03:05:50 +0900

    Add files in w10: gevent handling

--- lecture/w10_Thread_and_Process/bubble_sort_gevent.py
+++ lecture/w10_Thread_and_Process/bubble_sort_gevent.py
@@ -1,7 +1,8 @@
 import random
-import sys
+from gevent import monkey
+from gevent.pool import Pool
 
-def swap_reference (sort_list):
+def swap (sort_list):
 	time =0
 	for i in range(1,len(sort_list)):
 		for j in range(1,len(sort_list)):
@@ -11,16 +12,18 @@ def swap_reference (sort_list):
 				sort_list[j-1] = temp
 				time+=1
 	
-	print"The sorted list is as follow:",sort_list
+	print"The sorted list is as follow:"
 	print"The total number of swap is ",time
 
+monkey.patch_all()
+pool = Pool(3)
 
-size_of_list=2000
-
-for i in range(5):
+number_of_loop = range(100)
+for i in number_of_loop:
+	size_of_list=1000
 	list=range(1,size_of_list+1)
 	random.shuffle(list)
-	print"The unsorted list is as follow:",list
-	swap_reference(list)
-
+	print"The unsorted list is as follow:"
+	pool.spawn(swap, list)
+	
 
--- lecture/w10_Thread_and_Process/bubble_sort_thread.py
+++ lecture/w10_Thread_and_Process/bubble_sort_thread.py
@@ -5,15 +5,15 @@ import threading
 
 def swap_reference (sort_list):
 	time =0
-	for i in range(1,len(list)):
-		for j in range(1,len(list)):
-			if list[j-1]>list[j]: 
+	for i in range(1,len(sort_list)):
+		for j in range(1,len(sort_list)):
+			if sort_list[j-1]>sort_list[j]: 
 				temp = sort_list[j]
 				sort_list[j] = sort_list[j-1]
 				sort_list[j-1] = temp
 				time+=1
 	
-	print"The sorted list is as follow:",list
+	print"The sorted list is as follow:",sort_list
 	print"The total number of swap is ",time
 
 threads = []
